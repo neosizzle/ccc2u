db944ab164c665cdddf90b406924927ec9767ea2
neosizzle <44501267+neosizzle@users.noreply.github.com>
2021-03-22 15:17:03 +0800

    about.html created

---
+++ public/js/about.js
@@ -0,0 +1,20 @@
+const bgBtn = document.getElementById("bgBtn")
+const teamBtn = document.getElementById("teamBtn")
+const milestoneBtn = document.getElementById("milestoneBtn")
+
+const contentIframe = document.getElementById("content-iframe")
+
+//renders bg.html to conent iframe once bgBtn is clicked
+bgBtn.addEventListener("click" , ()=>{
+    contentIframe.src = "./bg.html"
+})
+
+//renders team.html to content iframe once teamBtn is clicked
+teamBtn.addEventListener("click" , ()=>{
+    contentIframe.src = "./team.html"
+})
+
+//renders milestone.html to content iframe once milestoneBtn is clicked
+milestoneBtn.addEventListener("click" , ()=>{
+    contentIframe.src = "./milestone.html"
+})
