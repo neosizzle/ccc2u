cd45a317da650174bd89a6a1fd47b34e510111d7
neosizzle <44501267+neosizzle@users.noreply.github.com>
2021-03-23 12:43:55 +0800

    finishing up products and services

--- public/js/about.js
+++ public/js/about.js
@@ -7,6 +7,7 @@ const contentIframe = document.getElementById("content-iframe")
 //renders bg.html to conent iframe once bgBtn is clicked
 bgBtn.addEventListener("click" , ()=>{
     contentIframe.src = "./bg.html"
+
 })
 
 //renders team.html to content iframe once teamBtn is clicked
