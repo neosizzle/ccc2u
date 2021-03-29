528b51a13e180107fee225379af51408f1833a02
neosizzle <44501267+neosizzle@users.noreply.github.com>
2021-03-29 14:01:31 +0800

    updated navbar in index

---
+++ public/js/scroll.js
@@ -0,0 +1,27 @@
+initNav = document.getElementById("init-nav-container");
+scrollNav = document.getElementById("scroll-nav");
+
+var myScrollFunc = function() {
+    var y = window.scrollY;
+    if (y >= 200) {
+      scrollNav.className = "navbar navbar-expand-lg navbar-light bg-light show"
+    } else {
+      scrollNav.className = "navbar navbar-expand-lg navbar-light bg-light hide"
+    }
+  };
+
+  if(screen.width > 800){
+    window.addEventListener("scroll", myScrollFunc);
+  }
+
+
+
+//checks if window resizes and executes adds or removes scroll listener according to screen size
+window.onresize = ()=>{
+    if(screen.width > 800){
+        window.addEventListener("scroll", myScrollFunc);
+      }else{
+        window.removeEventListener("scroll", myScrollFunc);
+      }
+}
+
