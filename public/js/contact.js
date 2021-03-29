3475272e2f9f8cfd9dc7369ae1493d8161b8667a
neosizzle <44501267+neosizzle@users.noreply.github.com>
2021-03-29 18:00:27 +0800

    progress on email

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,15 +1,57 @@
 form = document.getElementById("enquiry-form")
-enquiryType = document.getElementById("enquiryType")
-
-const myFunc = ()=>{
 
 
 
-    if(enquiryType.value == 'default'){
+form.addEventListener("submit" , (event)=>{
+
+    //prevent page from refreshing
+    event.preventDefault()
+
+    //get values from the form
+    enquiryType = document.getElementById("enquiryType").value
+    nameInput = document.getElementById("nameInput").value
+    emailInput = document.getElementById("emailInput").value
+    subjectInput = document.getElementById("subjectInput").value
+    messageInput = document.getElementById("messageInput").value
+
+
+    //create message obj to send to server
+    messageObj = {
+        "enquiryType" : enquiryType,
+        "name" : nameInput,
+        "email" : emailInput,
+        "subject" : subjectInput,
+        "message" : messageInput
+    }
+    
+    //validate enquiry type
+    if(enquiryType == 'default'){
         return alert("enquiry type cant be default")
     }
 
-    alert("form succ")
-}
 
-form.addEventListener("submit" , myFunc() )
+    //send post request to server
+
+    url = '/sendEmail'
+    params = {
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(messageObj),
+        method : "POST"
+    }
+
+    fetch(url , params)
+    .then(response => response.json())
+    .then((data)=>{
+        if(data.error){
+            alert(data.error)
+        }else{
+            alert("succ")
+        }
+    })
+
+
+
+
+} )
--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const path = require('path')
 require('dotenv').config({path: __dirname + '/.env'})
 const app = express()
+app.use(express.json());
 
 require('./db/mongoose')//We dont want to grab anything, just wanted to make sure it runs
 
@@ -15,12 +16,40 @@ const port = process.env.PORT || 3000
 //declares express public directory path and tells the app to use that path to serve up static assets such as html
 const publicDirPath = path.join(__dirname, '../public')
 
+// // Express bellow v4.16.0
+// // ----------------------
+// const bodyParser = require('body-parser');
+
+// app.use(body.json());
+// app.use(express.urlencoded({
+//   extended: true
+// }));
+
 app.use(express.static(publicDirPath))
 
 
 //index endpoint
 app.get('' , (req,res)=>{
     res.render("../public/index.html")
+})
+
+//email send endpoint
+app.post('/sendEmail' ,(req,res)=>{
+    console.log(typeof req)
+
+    //send object to api
+    if(req.body.name == 'jack'){
+        console.log('reached')
+        res.status(500).send({
+            error : 'jack bad'
+        })
+
+        return
+    }
+
+    //everything goes well, send back json response without error 
+    res.status(200).json({think : "good"})
+    
 })
 
 //The 404 Route (ALWAYS Keep this as the last route)
