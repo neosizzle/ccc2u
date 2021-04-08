973c5074b50a33a350bab7bc8176aac38c4d8ad2
unknown <junhanngd1115@gmail.com>
2021-04-08 14:39:03 +0800

    updated receiver email

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.post('/sendEmail' ,(req,res)=>{
       //send from transporter to receiver
       const mailOptions = {
         from: process.env.NODEMAILER_TRANS_EMAIL,
-        to: 'jh_ng@ymail.com',//receiver email
+        to: 'enquiry@ccc2u.com',//receiver email
         subject: 'Ccc2u customer email enquiry received ',
       };
 
